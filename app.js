d44dd604c72bb0e854313c139c40fbbc29d4f874
tangelia <tangelia.atlanta@gmail.com>
2019-04-10 17:46:23 -0400

    First commit pseudocode

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //balance declarations here
+    
+    
+    //  Place event listeners here
+    
+
+    //Setup board
+    
+    //Setup variables
+    //Set up Game loop
+        //Show board
+        //Setup Player One
+        //Setup Player Two
+        //Allow for first player to move
+        //check for win
+        //Allow for second player to move
+        //check for win
+        // check for wins looping over the board
+
+
+}),
