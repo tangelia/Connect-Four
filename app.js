a953c097ae219c38764d14bb5b4e2b193cb75cff
tangelia <tangelia.atlanta@gmail.com>
2019-04-11 11:57:35 -0400

    Background setup

--- app.js
+++ app.js
@@ -8,19 +8,46 @@ $(document).ready(function(){
     //Setup board
     
     //Setup variables
+
     //Set up Game loop
+    //  a button to start the game
         //Show board
         //Setup Player One
         //Setup Player Two
         //Allow for first player to move
+        // hover for move
         //make the move
         //check for win
+        // function horizontalWin{
+
+        // }
+        // function verticalWin{
+
+        // }
+        // function diagonalWinLeft{
+
+        // }
+        // function diagonalWinRight{
+
+        // }
+        //Switch Player
         //Allow for second player to move
         //make the move
         //check for win
         // check for wins looping over the board
         //switch players
-        //
+        //Win modal
+        // function Win{
+
+        // }
+        //Lose modal
+        // function Lose{
+
+        // }
+        // Exit game modal
+        // function exitGame{
+
+        // }
 
 
 }),
---
+++ app2.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    class gameBoard{
+        constructor(){
+            this.rows = 6
+            this.col = 7
+        }
+    }
+});
