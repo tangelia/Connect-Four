a02f457657148d0631217ccbcf4627eab4c513f6
tangelia <tangelia.atlanta@gmail.com>
2019-04-14 11:14:17 -0400

    Debugging Uncaught Typeface Error and resized board

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 //Setup variables
 let currentPlayer = "coral";
-
-//Show board
+let grid = [];
+//Show board and restart board
 
 $(document).ready(function() {
-  let grid = []
-
-  restart =()=>{
+  restart = () => {
     grid = [
-      [{}, {}, {}, {}, {}, {}, {}],
       [{}, {}, {}, {}, {}, {}, {}],
       [{}, {}, {}, {}, {}, {}, {}],
       [{}, {}, {}, {}, {}, {}, {}],
@@ -18,7 +15,7 @@ $(document).ready(function() {
     ];
 
     render();
-  }
+  };
 
   const render = () => {
     const svg = document.getElementById("svg");
@@ -41,17 +38,16 @@ $(document).ready(function() {
     }
     svg.innerHTML = doc;
   };
-  //  Set up Player moves,
-  // Allow for first player to move
-  // switches between player's colors
-  // Set color in last cell
-  window.clickCircle = (x, y) => {
+  /*  Set up Player moves,
+     Allow for first player to move
+     switches between player's colors
+     Set color in last cell*/
+  window.clickCircle = (x, y) =>{
     for (let i = grid.length - 1; i >= 0; i--) {
       let row = grid[i];
       let lastEmptyCircle = row[x];
-
       if (!lastEmptyCircle.color) {
-        row[x] = { color: currentPlayer };
+        row[x] = { color:currentPlayer };
         //switches color to siginify player
         currentPlayer = currentPlayer === "coral" ? "black" : "coral";
         render();
@@ -60,111 +56,124 @@ $(document).ready(function() {
       }
     }
   };
-  //Set up Game loop
-  //  a button to start the game
-
-  //make the move
-  // check for win
+  /*Set up Game loop
+    make the move
+    check for win*/
   function winner(){
     console.log("Who won?");
-    // check for wins looping over the board
-    // checking horizontal
-    for (let i = 0; i < grid.length; i++) {
-      let row = grid[i];
-      for (let j = 0; j < row.length; j++) {
-        let circle = grid[i][j];
-        if (circle && circle.color) {
-          // Checks for horizontal win
-          if (j === 0 || j === 1 || j === 2 || j === 3) {
-            if (
-              grid[i][j + 1].color === circle.color &&
-              grid[i][j + 2].color === circle.color &&
-              grid[i][j + 3].color === circle.color
-            ) {
-              alert("Color " + circle.color + " wins!");
-              return;
-            }
-          }
-          if (j === 0 || j === 1) {
-            if (
-              grid[i + 1][j].color === circle.color &&
-              grid[i + 2][j].color === circle.color &&
-              grid[i + 3][j].color === circle.color
-            ) {
-              alert("Color " + circle.color + " wins!");
-              return;
-            }
+  //check for wins looping over the board
+    veritcalWin();
+    debugger;
+    // horizontalWin();
+    // diagonalWinRight();
+    // diagonalWinLeft();
+    return;
+  };
+
+  restart();
+
+
+
+function veritcalWin() {
+  for (let i = 0; i < grid.length; i++) {
+    let row = grid[i];
+    for (let j = 0; j < row.length; j++) {
+      let circle = grid[i][j];
+      if (circle && circle.color) {
+        if (j === 0 || j === 1) {
+          if (
+            grid[i + 1][j].color === circle.color &&
+            grid[i + 2][j].color === circle.color &&
+            grid[i + 3][j].color === circle.color
+          ) {
+            alert("Color "+ circle.color + " wins!");
           }
         }
       }
     }
   }
-  restart ();
-  
-  // function playMusic(){
-  //   let music = new Audio('FKJ - Tui.mp3');
-  //   music.play();
-  //   }
-});
-// // function veritcalWin(){
+};
+
+// function horizontalWin() {
 //   for (let i = 0; i < grid.length; i++) {
-//        let row = grid[i];
-//        for (let j = 0; j < row.length; j++) {
-//          let circle = grid[i][j];
-//         if (circle && circle.color){
-//           if (j === 0 || j === 1){
-//               if (grid[i+1][j].color === circle.color &&
-//                 grid[i+2][j].color === circle.color &&
-//                 grid[i+3][j].color === circle.color){
-//                     alert("Color", circle.color,"wins!");
-//                 }
-//             }
-//            }
-//       }
-//    }
-// / }
-// function diagonalWinLeft(){
-//for (let i = 0; i < grid.length; i++) {
 //     let row = grid[i];
 //     for (let j = 0; j < row.length; j++) {
 //       let circle = grid[i][j];
-// }
-// function diagonalWinRight (){
-//for (let i = 0; i < grid.length; i++) {
-//   //     let row = grid[i];
-//   //     for (let j = 0; j < row.length; j++) {
-//   //       let circle = grid[i][j];
-// }
+//       if (circle && circle.color) {
+//         if (j === 0 || j === 1 || j === 2 || j === 3) {
+//           if (
+//             grid[i][j + 1].color === circle.color &&
+//             grid[i][j + 2].color === circle.color &&
+//             grid[i][j + 3].color === circle.color
+//           ) {
+//             alert("Color " + circle.color + " wins!");
+//             return;
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
 
-// check for wins looping over the board
-// function horizontalWin(){
+// function diagonalWinRight() {
+//   for (let i = 0; i < grid.length; i++) {
+//     let row = grid[i];
+//     for (let j = 0; j < row.length; j++) {
+//       let circle = grid[i][j];
+//       if (circle && circle.color) {
+//         if (j === 3 || j === 4 || j === 5 || j === 6) {
+//           if (
+//             grid[i + 1][j - 1].color === circle.color &&
+//             grid[i + 2][j - 2].color === circle.color &&
+//             grid[i + 3][j - 3].color === circle.color
+//           ) {
+//             alert("Color " + circle.color + " wins!");
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
 
-// function veritcalWin(){
-
-// function diagonalWinLeft(){
-
-// }
-// function diagonalWinRight (){
-
-//   render();
-// });
+// function diagonalWinLeft() {
+//   for (let i = 0; i < grid.length; i++) {
+//     let row = grid[i];
+//     for (let j = 0; j < row.length; j++) {
+//       let circle = grid[i][j];
+//       if (circle && circle.color) {
+//         if (i === 0 || i === 1) {
+//           if (j === 0 || j === 1 || j === 2 || j === 3){
+//             if (
+//               grid[i + 1][j + 1].color === circle.color &&
+//               grid[i + 2][j + 2].color === circle.color &&
+//               grid[i + 3][j + 3].color === circle.color
+//             ) {
+//               alert("Color "+ circle.color + " wins!");
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
 
 //Win modal
 // function Win (){
 
 // }
-//Lose modal
-// function Lose{
 
-// }
 // Exit game modal
 // function exitGame{
 
 // }
+// function playMusic(){
+  //   let music = new Audio('FKJ - Tui.mp3');
+  //   music.play();
+  //   }
 // play music
 // function playMusic(){
 //     let music = new Audio('FKJ - Tui.mp3');
 //     music.play();
 //     }
 
-// });
+});
